7745c56cae68f7c7518ba06eb76009456123fb11
Richard Siwady <rsiwady29@gmail.com>
2014-11-14 16:02:13 -0600

    clamping scale values elegantly

--- src/JIC.js
+++ src/JIC.js
@@ -32,17 +32,14 @@ var jic = {
                 mime_type = "image/png";
              }
              
-             scale = scale < 0 ? 0 
-                        : scale > 100 ? 100
-                            :scale;
-
+             scale = Math.min(Math.max(scale, 0), 100);
              var cvs = document.createElement('canvas');
              cvs.width = scale === undefined ? source_img_obj.naturalWidth
                             : (scale/100) * source_img_obj.naturalWidth;
 
              cvs.height = scale === undefined ? source_img_obj.naturalHeight
                             : (scale/100) * source_img_obj.naturalHeight;
-                                                        
+
              var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0);
              var newImageData = cvs.toDataURL(mime_type, quality/100);
              var result_image_obj = new Image();
